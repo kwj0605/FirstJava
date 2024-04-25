ea999dd9adf775356e6a85dac8891694201910bc
Woojin Kim <rladnwlsok@gmail.com>
2024-04-25 20:20:04 +0900

    자바 강의 2차시 숙제

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,70 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner sc =new Scanner(System.in);
+        System.out.println("자료구조명을 입력하세요(List, Set, Map)");
+        String dataStructure = sc.nextLine();
         System.out.println("요리 제목을 입력하세요");
-        String title = sc.nextLine();
-        System.out.println("별점을 입력하세요(1~5)");
-        String star = sc.nextLine();  //nextDouble에서 수정 \n
-        System.out.println("요리 레시피를 10문장 입력하세요");
-        String[] recipe = new String[10];
-        for(int i=0; i<10; i++) {
-            recipe[i] = sc.nextLine();
+        String cooking = sc.nextLine();
+        System.out.println("요리 레시피를 입력하세요(입력을 마쳣다면 마지막에 \"끝\"을 입력하세요)");
+
+
+        switch (dataStructure) {
+            case "List" :
+                ArrayList<String> textList = new ArrayList<String>();
+                while (true) {
+                    String a = sc.nextLine();
+                    if(!a.equals("끝")) {
+                        textList.add(a);
+                    }
+                    else break;
+                }
+                System.out.println("[ "+dataStructure +"으로 저장된 "+ cooking+" 만들기 ]");
+                int i=1;
+                for (String listValue : textList) {
+                    System.out.println(i+". "+ listValue);
+                    i++;
+                }
+                textList.clear();
+            case "Set" :    //hashset은 무작위로 저장하기 때문에 순서대로 저장하는 linkedhashset을 사용
+                Set<String> textSet = new LinkedHashSet<String>();
+                while (true) {
+                    String a = sc.nextLine();
+                    if(!a.equals("끝")) {
+                        textSet.add(a);
+                    }
+                    else break;
+                }
+                System.out.println("[ "+dataStructure +"으로 저장된 "+ cooking+" 만들기 ]");
+                i = 1;
+                for (String setValue : textSet) {
+                    System.out.println(i+". "+ setValue);
+                    i++;
+                }
+                textSet.clear();
+            case "Map" :
+                Map<Integer, String > textMap = new HashMap<Integer, String>();
+                i=0;
+                while (true) {
+                    String a = sc.nextLine();
+                    if(!a.equals("끝")) {
+                        textMap.put(i, a);
+                        i++;
+                    }
+                    else break;
+                }
+                System.out.println("[ "+dataStructure +"으로 저장된 "+ cooking+" 만들기 ]");
+                i=1;
+                for (String mapValue : textMap.values()) {
+                    System.out.println(i+". "+ mapValue);
+                    i++;
+                }
+                textMap.clear();
         }
-
-        //퍼센트 계산
-        int intStar = (int)Float.parseFloat(star);
-        int percent = (intStar*100/5);       //(int)star/5*100;
-        System.out.println(percent);
-
-
-        //출력 부분
-        System.out.println("[ "+title+" ]");
-        System.out.println("별점 : "+intStar+" ("+percent+"%)");
-        for(int j=1; j<=10; j++) {
-            System.out.println(j+". "+recipe[j-1]);
-        }
+        sc.close();
+        return;
     }
 
 }
