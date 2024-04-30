3eb61488ae3f5dfc564b516aeb69247ab18113de
Woojin Kim <rladnwlsok@gmail.com>
2024-04-30 17:04:10 +0900

    level2 요구사항 6번까지

--- src/Level2/App.java
+++ src/Level2/App.java
@@ -1,19 +1,38 @@
 package Level2;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        int num1=1, num2=0;
-        char operationSymbol='/';
-        String msg;
-        Calculator cal = new Calculator();
-        try {
-            double a = cal.calculate(num1, num2, operationSymbol);
-            System.out.println(a);
-            Exception e = new Exception("분모가 0입니다.");
-            throw e;
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-        }
+        Calculator calculator = new Calculator();
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("첫 번째 숫자를 입력하세요:");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요:");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            try {       // 숫자 2개와 연산기호 넣고 계산
+                double calculationResult = calculator.calculate(num1, num2, operator);
+                calculator.setResultList(calculationResult);
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            if (sc.nextLine().equals("remove")) calculator.removeResult();
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            if (sc.nextLine().equals("inquiry")) calculator.inquiryResults();
+
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        } while(! sc.nextLine().equals("exit"));
+
     }
 }
--- src/Level2/Calculator.java
+++ src/Level2/Calculator.java
@@ -1,22 +1,29 @@
 package Level2;
 
-public class Calculator {
-    public Calculator() {}; // 생성자
-    double result=0;    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
-    double operationResult;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Double calculate(int num1, int num2, char operationSymbol) throws Exception {
+public class Calculator {
+    double result = 0;    /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
+
+    private List<Double> resultList;  /* 연산 결과를 저장하는 컬렉션 타입 필드를 외부에서 직접 접근 하지 못하도록 수정*/
+
+    public Calculator() {
+        new ArrayList<Double>();
+    }
+
+    public double calculate(int num1, int num2, char operationSymbol) throws Exception {
         switch (operationSymbol) {
             case '+':
-                result = (double)num1 + num2;
+                result = (double) num1 + num2;
                 break;
 
             case '-':
-                result = (double)num1 - num2;
+                result = (double) num1 - num2;
                 break;
 
             case '*':
-                result = (double)num1 * num2;
+                result = (double) num1 * num2;
                 break;
 
             case '/':
@@ -24,10 +31,39 @@ public class Calculator {
                     Exception e = new Exception("분모가 0입니다.");
                     throw e;
                 }
-
-                result = (double)num1 / num2;
+                result = (double) num1 / num2;
                 break;
         }
         return result;
     }
+
+    public double getResultList(int i) {        // i번째 list요소 출력
+        return this.resultList.get(i);
+    }        /* Getter 메서드 구현 */
+
+    public void setResultList(double calculationResult) {   // list에 연산결과 추가
+        this.resultList.add(calculationResult);
+    }            /* Setter 메서드 구현 */
+
+    public void removeResult() {    /* 리스트의 가장 첫 번째 삭제 */
+        resultList.remove(0);
+    }
+
+    public void inquiryResults() {  /* 연산 결과들을 조회 */
+        for (double k:resultList)
+            System.out.println(k);
+    }
+
+
+    /* static, final 활용 */
+    /* 원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성 */
+    /* 생성자 수정 */
+    static final double pi = Math.PI;
+
+    /* 원의 넓이를 구하는 메서드 선언*/
+    public double calculateCircleArea(매배변수) {
+        /* 원의 넓이 계산 구현 */
+    }
+    /* 원의 넓이 저장 필드 Getter, Setter, 조회 메서드 구현 */
+
 }
